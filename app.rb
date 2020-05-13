65a8f795ac19d968374ccc3aad08f1a4e9c529b3
Stanis1av <krasavin.stanislaus@gmail.com>
2020-05-13 22:08:36 +0400

    Add navbar

--- app.rb
+++ app.rb
@@ -2,43 +2,25 @@ require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 
-configure do
-   enable :sessions
- end
 
- helpers do
-   def username
-     session[:identity] ? session[:identity] : 'Hello stranger'
-   end
- end
+    get '/' do
+      erb :home#'This is a my <a href="https://github.com/Stanis1av">Github</a>?'
+    end
 
- before '/secure/*' do
-   unless session[:identity]
-     session[:previous_url] = request.path
-     @error = 'Sorry, you need to be logged in to visit ' + request.path
-     halt erb(:login_form)
-   end
- end
+    get '/home' do
+      erb :home
+    end
 
- get '/' do
-   erb 'Can you handle a <a href="/secure/place">secret</a>?'
- end
+    get '/about' do
+      erb :about
+    end
 
- get '/login/form' do
-   erb :login_form
- end
+    get '/visit' do
+      erb :visit
+    end
 
- post '/login/attempt' do
-   session[:identity] = params['username']
-   where_user_came_from = session[:previous_url] || '/'
-   redirect to where_user_came_from
- end
+    get '/contacts' do
+      erb :contacts
+    end
 
- get '/logout' do
-   session.delete(:identity)
-   erb "<div class='alert alert-message'>Logged out</div>"
- end
 
- get '/secure/place' do
-   erb 'This is a secret place that only <%=session[:identity]%> has access to!'
- end
